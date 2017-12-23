558c972bd6f0e8b8a824062a030cb170cf751b8a
Matt Levin <levinmat@gmail.com>
2017-12-22 19:11:05 -0500

    animation

--- js/home.js
+++ js/home.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
 
     // Interests Section
     // Initial Fade in
-    $("#slot0").fadeOut(0);
-    $("#slot1").fadeOut(0);
-    $("#slot2").fadeOut(0);
-    $("#slot0").fadeIn(2000);
-    $("#slot1").fadeIn(2000);
-    $("#slot2").fadeIn(2000);
+    $("#slot0").animate({opacity: 0}, duration=20);
+    $("#slot1").animate({opacity: 0}, duration=20);
+    $("#slot2").animate({opacity: 0}, duration=20);
+    $("#slot0").animate({opacity: 1});
+    $("#slot1").animate({opacity: 1});
+    $("#slot2").animate({opacity: 1});
 
     // Swap interest every 2000 ms
     setInterval(swap, 2000);
@@ -24,10 +24,10 @@ var s = 0; // Slot index
 function swap() {
     //  Get current slot and increment slot number
     var slot = $("#slot" + (s++ % 3));
-    slot.fadeOut(500); // Fade out
+    slot.animate({opacity: 0}); // Fade out
     setTimeout(function() { // Wait until fully disappeared...
         // Set the text of curr slot to curr interest and increment i
         slot.text(interests[i++ % interests.length]);
-        slot.fadeIn(600); // Fade in with new text
+        slot.animate({opacity: 1}); // Fade in with new text
     }, 500)
 }
